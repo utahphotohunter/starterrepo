3a554cb8762cee965f376c9a3019a31247898773
utahphotohunter <mitch.hunter1197@gmail.com>
2025-01-23 23:20:37 -0700

    Create relationship between 'classification' and 'inventory' tables

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -27,3 +27,10 @@ CREATE TABLE IF NOT EXISTS public.inventory
 	classification_id integer NOT NULL,
 	CONSTRAINT inventory_pkey PRIMARY KEY (inv_id)
 );
+
+-- Create relationship between 'classification' and 'inventory' tables
+ALTER TABLE IF EXISTS public.inventory
+	ADD CONSTRAINT fk_classification FOREIGN KEY (classification_id)
+	REFERENCES public.classification (classification_id) MATCH SIMPLE
+	ON UPDATE CASCADE
+	ON DELETE NO ACTION;
