f5900bf03f56f3782cfe5bc5cfb73a64fe582647
utahphotohunter <mitch.hunter1197@gmail.com>
2025-02-03 21:31:29 -0700

    finished the second assigment for w0

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const app = express()
 const static = require("./routes/static")
 const expressLayouts = require("express-ejs-layouts")
 const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 /* ***********************
  * View Engine and Templates
@@ -25,6 +26,8 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
 // Index route
 app.get("/", baseController.buildHome)
+// Inventory routes
+app.use("/inv", inventoryRoute)
 
 
 /* ***********************
