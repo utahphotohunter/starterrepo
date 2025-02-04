c5d032d6d5edda9161c17e29b3769a3ad9c96ee8
utahphotohunter <mitch.hunter1197@gmail.com>
2025-02-03 20:43:36 -0700

    finished the first w04 assignment

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const expressLayouts = require("express-ejs-layouts")
+const baseController = require("./controllers/baseController")
 
 /* ***********************
  * View Engine and Templates
@@ -23,9 +24,7 @@ app.set("layout", "./layouts/layout") // not at views root
  *************************/
 app.use(static)
 // Index route
-app.get("/", function(req, res){
-    res.render("index", {title: "Home"});
-});
+app.get("/", baseController.buildHome)
 
 
 /* ***********************
